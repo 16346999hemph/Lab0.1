54fdcb56f0297ccf331b0ea1abb32936f09b7023
16346999hemph <ahvdr@umsystem.edu>
2025-06-10 17:29:56 -0500

    Add files via upload

---
+++ Lab0.1/ascending.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int valA, valB, valC, valD; // Declare four integer values
+	int temp; 
+
+	cout << "Please enter four integers:" << endl;
+	cout << "Value A: ";
+	cin >> valA;
+	cout << "Value B: ";
+	cin >> valB;
+	cout << "Value C: ";
+	cin >> valC;
+	cout << "Value D: ";
+	cin >> valD;
+
+	// Sort in Ascending Order
+
+	// First iteration
+	if (valA > valB) {
+		temp = valA;
+		valA = valB;
+		valB = temp;
+	}
+	if (valB > valC) {
+		temp = valB;
+		valB = valC;
+		valC = temp;
+	}
+	if (valC > valD) {
+		temp = valC;
+		valC = valD;
+		valD = temp;
+	}
+
+	// Second iteration
+	if (valA > valB) {
+		temp = valA;
+		valA = valB;
+		valB = temp;
+	}
+	if (valB > valC) {
+		temp = valB;
+		valB = valC;
+		valC = temp;
+	}
+
+	// Third iteration
+	if (valA > valB) {
+		temp = valA;
+		valA = valB;
+		valB = temp;
+	}
+
+	// Output the sorted array
+	cout << "\nSorted values in ascending order: ";
+	cout << valA << ", " << valB << ", " << valC << ", " << valD;
+
+	return 0;
+}
