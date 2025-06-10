8f0891c7e8d6fc2ac3dabb33e5c30f21d549d4bf
cyber <cyber@LAPTOP-NT2DFRJV>
2025-06-10 15:37:10 -0500

    Add project files.

---
+++ Lab0.1/Largest.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c, d, large;
+	char again = 'Y';
+	while (again == 'Y') {
+		cout << "Enter 4 integers: ";
+		cin >> a >> b >> c>> d;
+		large = a;
+		if (b > large) {
+			large = b;
+		}
+		if (c > large) {
+			large = c;
+		}
+		if (d > large) {
+			large = d;
+		}
+		cout << "The largest number is: " <<
+			large;
+		cout << " Again? ";
+		cin >> again;
+	}
+	return 0;
+}
