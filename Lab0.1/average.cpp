9eb0121fc07ef5dee0d273695449caf5b069fc5e
16346999hemph <ahvdr@umsystem.edu>
2025-06-10 17:17:49 -0500

    Add files via upload

---
+++ Lab0.1/average.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+	{
+		int a, b, c, d;
+		int sum = 0;
+		cout << "This is an average calculator. \n";
+		cout << "Please enter 4 numbers:\n";
+		cin >> a >> b >> c >> d;
+		sum = a + b + c + d;
+		cout << "Sum of the numbers: " << sum << endl;
+		float avg = sum / static_cast<double>(4);
+		cout << "Average of the  numbers: " << avg << endl;
+		return 0;
+	}
